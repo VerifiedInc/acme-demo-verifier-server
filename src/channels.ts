acdb4137d02050f85dbd0c3fab82307b011273d7
jacobleesinger <jacobleesinger@gmail.com>
2021-08-04 17:55:25 -0700

    [channels] log on connection

--- src/channels.ts
+++ src/channels.ts
@@ -22,6 +22,7 @@ export default function (app: Application): void {
   }
 
   app.on('connection', (connection: any): void => {
+    logger.info('on connection', connection);
     // On a new real-time connection, add it to the anonymous channel
     app.channel('anonymous').join(connection);
   });
