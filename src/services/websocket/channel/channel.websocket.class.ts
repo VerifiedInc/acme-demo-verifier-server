b544d2da9fbb5da5a307a134bde2ffacf65383c0
jacobleesinger <jacobleesinger@gmail.com>
2021-08-05 11:26:03 -0700

    [ChannelService] add tests

--- src/services/websocket/channel/channel.websocket.class.ts
+++ src/services/websocket/channel/channel.websocket.class.ts
@@ -8,6 +8,18 @@ interface ServiceOptions { }
 export class ChannelService {
   private app: Application;
   private options: ServiceOptions;
+
+  /**
+   * Add the client connection to a specified channel.
+   * If the channel does not exist, it will be created.
+   *
+   * Primarily used to re-join previous session channel after a disconnect
+   *
+   * @param {{ channel: string}} data
+   * @param {Params} params
+   * @returns {Promise<{ success: boolean }>}
+   * @memberof ChannelService
+   */
   async create (data: { channel: string}, params: Params): Promise<{ success: boolean }> {
     logger.info('channelService create');
     const { channel } = data;
